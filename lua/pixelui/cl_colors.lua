2dba3d9493594170849cef51d8d52dc9837bc157
TomDotBat <tom@tomdotbat.dev>
2020-12-17 18:54:36 +0000

    Add a global colour table

---
+++ lua/pixelui/cl_colors.lua
@@ -0,0 +1,13 @@
+
+PIXEL.Colors = {
+    Background = Color(22, 22, 22),
+    Header = Color(28, 28, 28),
+
+    PrimaryText = Color(255, 255, 255),
+    SecondaryText = Color(220, 220, 220),
+    DisabledText = Color(100, 100, 100),
+
+    Primary = Color(43, 93, 114),
+    Positive = Color(66, 134, 50),
+    Negative = Color(164, 50, 50)
+}
