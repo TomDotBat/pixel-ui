62a9580c281a36604bd400a256221eb2e41cfddf
TomDotBat <tom@tomdotbat.dev>
2020-12-20 15:34:08 +0000

    Overhead drawing helper

---
+++ lua/pixelui/common/cl_overheads.lua
@@ -0,0 +1,66 @@
+
+PIXEL.RegisterFontUnscaled("Overhead", "Open Sans Bold", 100)
+
+local localPly
+local function checkDistance(ent)
+    if not IsValid(localPly) then localPly = LocalPlayer() end
+    if localPly:GetPos():DistToSqr(ent:GetPos()) > 200000 then return true end
+end
+
+local function drawOverhead(ent, pos, text, ang)
+    if ang then
+        ang = ent:LocalToWorldAngles(ang)
+    else
+        ang = localPly:EyeAngles()
+        ang:SetUnpacked(0, ang[2] - 90, 90)
+    end
+
+    surface.SetFont("PIXEL.Overhead")
+    local w, h = surface.GetTextSize(text)
+    w = w + 40
+    h = h + 6
+
+    local x, y = -(w * .5), -h
+
+    local oldClipping = DisableClipping(true)
+
+    cam.Start3D2D(pos, ang, 0.05)
+        PIXEL.DrawRoundedBox(4, x, y, w, h, PIXEL.Colors.Primary)
+        PIXEL.DrawText(text, "PIXEL.Overhead", 0, y + 1, PIXEL.Colors.PrimaryText, TEXT_ALIGN_CENTER)
+    cam.End3D2D()
+
+    DisableClipping(oldClipping)
+end
+
+local entOffset = 2
+function PIXEL.DrawEntOverhead(ent, text, angleOverride, posOverride)
+    if checkDistance(ent) then return end
+
+    if posOverride then
+        drawOverhead(ent, ent:LocalToWorld(posOverride), text, angleOverride)
+        return
+    end
+
+    local pos = ent:OBBMaxs()
+    pos:SetUnpacked(0, 0, pos[3] + entOffset)
+
+    drawOverhead(ent, ent:LocalToWorld(pos), text, angleOverride)
+end
+
+local eyeOffset = Vector(0, 0, 7)
+local fallbackOffset = Vector(0, 0, 73)
+function PIXEL.DrawNPCOverhead(ent, text, angleOverride, offsetOverride)
+    if checkDistance(ent) then return end
+
+    local eyeId = ent:LookupAttachment("eyes")
+    if eyeId then
+        local eyes = ent:GetAttachment(eyeId)
+        if eyes then
+            eyes.Pos:Add(offsetOverride or eyeOffset)
+            drawOverhead(ent, eyes.Pos, text, angleOverride)
+            return
+        end
+    end
+
+    drawOverhead(ent, ent:GetPos() + fallbackOffset, text, angleOverride)
+end
