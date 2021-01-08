8cfe31643407077dfa2e3de0f551642a0ee50a5e
Tom O'Sullivan <tom@tomdotbat.dev>
2021-01-08 16:14:44 +0000

    Add a menu element

---
+++ lua/pixelui/elements/cl_menu.lua
@@ -0,0 +1,201 @@
+
+local PANEL = {}
+
+AccessorFunc(PANEL, "m_bBorder", "DrawBorder")
+AccessorFunc(PANEL, "m_bDeleteSelf", "DeleteSelf")
+AccessorFunc(PANEL, "m_iMinimumWidth", "MinimumWidth")
+AccessorFunc(PANEL, "m_bDrawColumn", "DrawColumn")
+AccessorFunc(PANEL, "m_iMaxHeight", "MaxHeight")
+AccessorFunc(PANEL, "m_pOpenSubMenu", "OpenSubMenu")
+
+function PANEL:Init()
+    self:SetIsMenu(true)
+    self:SetDrawBorder(true)
+    self:SetPaintBackground(true)
+    self:SetMinimumWidth(PIXEL.Scale(100))
+    self:SetDrawOnTop(true)
+    self:SetMaxHeight(ScrH() * 0.3)
+    self:SetDeleteSelf(true)
+    self:SetBarDockShouldOffset(true)
+
+    self:SetPadding(0)
+
+    self.BackgroundCol = PIXEL.OffsetColor(PIXEL.Colors.Background, 10)
+
+    RegisterDermaMenuForClose(self)
+end
+
+function PANEL:AddPanel(pnl)
+    self:AddItem(pnl)
+    pnl.ParentMenu = self
+end
+
+function PANEL:AddOption(strText, funcFunction)
+    local pnl = vgui.Create("PIXEL.MenuOption", self)
+    pnl:SetMenu(self)
+    pnl:SetText(strText)
+    if funcFunction then pnl.DoClick = funcFunction end
+
+    self:AddPanel(pnl)
+
+    return pnl
+end
+
+function PANEL:AddCVar(strText, convar, on, off, funcFunction)
+    local pnl = vgui.Create("PIXEL.MenuOptionCVar", self)
+    pnl:SetMenu(self)
+    pnl:SetText(strText)
+    if funcFunction then pnl.DoClick = funcFunction end
+
+    pnl:SetConVar(convar)
+    pnl:SetValueOn(on)
+    pnl:SetValueOff(off)
+
+    self:AddPanel(pnl)
+
+    return pnl
+end
+
+function PANEL:AddSpacer(text, func)
+    local pnl = vgui.Create("Panel", self)
+
+    pnl.Paint = function(p, w, h)
+        surface.SetDrawColor(PIXEL.Colors.PrimaryText)
+        surface.DrawRect(0, 0, w, h)
+    end
+
+    pnl:SetTall(PIXEL.Scale(2))
+    self:AddPanel(pnl)
+
+    return pnl
+end
+
+function PANEL:AddSubMenu(strText, funcFunction)
+    local pnl = vgui.Create("PIXEL.MenuOption", self)
+    local subMenu = pnl:AddSubMenu(strText, funcFunction)
+
+    pnl:SetText(strText)
+    if funcFunction then pnl.DoClick = funcFunction end
+
+    self:AddPanel(pnl)
+
+    return subMenu, pnl
+end
+
+function PANEL:Hide()
+    local openmenu = self:GetOpenSubMenu()
+    if openmenu then
+        openmenu:Hide()
+    end
+
+    self:SetVisible(false)
+    self:SetOpenSubMenu(nil)
+end
+
+function PANEL:OpenSubMenu(item, menu)
+    local openmenu = self:GetOpenSubMenu()
+    if IsValid(openmenu) and openmenu:IsVisible() then
+        if menu and openmenu == menu then return end
+
+        self:CloseSubMenu(openmenu)
+    end
+
+    if not IsValid(menu) then return end
+
+    local x, y = item:LocalToScreen(self:GetWide(), 0)
+    menu:Open(x, y, false, item)
+
+    self:SetOpenSubMenu(menu)
+end
+
+function PANEL:CloseSubMenu(menu)
+    menu:Hide()
+    self:SetOpenSubMenu(nil)
+end
+
+function PANEL:Paint(w, h)
+    PIXEL.DrawRoundedBox(PIXEL.Scale(4), 0, 0, w, h, self.BackgroundCol)
+end
+
+function PANEL:ChildCount()
+    return #self:GetCanvas():GetChildren()
+end
+
+function PANEL:GetChild(num)
+    return self:GetCanvas():GetChildren()[num]
+end
+
+function PANEL:LayoutContent(w, h)
+    w = self:GetMinimumWidth()
+
+    local children = self:GetCanvas():GetChildren()
+    for k, pnl in pairs(children) do
+        pnl:InvalidateLayout(true)
+        w = math.max(w, pnl:GetWide())
+    end
+
+    self:SetWide(w)
+
+    local y = 0
+    for k, pnl in pairs(children) do
+        pnl:SetWide(w)
+        pnl:SetPos(0, y)
+        pnl:InvalidateLayout(true)
+
+        y = y + pnl:GetTall()
+    end
+
+    y = math.min(y, self:GetMaxHeight())
+
+    self:SetTall(y)
+end
+
+function PANEL:Open(x, y, skipanimation, ownerpanel)
+    RegisterDermaMenuForClose(self)
+
+    local maunal = x and y
+    x = x or gui.MouseX()
+    y = y or gui.MouseY()
+
+    local ownerHeight = 0
+    if ownerpanel then ownerHeight = ownerpanel:GetTall() end
+
+    self:InvalidateLayout(true)
+
+    local w, h = self:GetWide(), self:GetTall()
+
+    self:SetSize(w, h)
+
+    if y + h > ScrH() then y = ((maunal and ScrH()) or (y + ownerHeight)) - h end
+    if x + w > ScrW() then x = ((maunal and ScrW()) or x) - w end
+    if y < 1 then y = 1 end
+    if x < 1 then x = 1 end
+
+    self:SetPos(x, y)
+
+    self:MakePopup()
+    self:SetVisible(true)
+    self:SetKeyboardInputEnabled(false)
+end
+
+function PANEL:OptionSelectedInternal(option)
+    self:OptionSelected(option, option:GetText())
+end
+
+function PANEL:OptionSelected(option, text) end
+
+function PANEL:ClearHighlights()
+    for k, pnl in pairs(self:GetCanvas():GetChildren()) do
+        pnl.Highlight = nil
+    end
+end
+
+function PANEL:HighlightItem(item)
+    for k, pnl in pairs(self:GetCanvas():GetChildren()) do
+        if pnl == item then
+            pnl.Highlight = true
+        end
+    end
+end
+
+vgui.Register("PIXEL.Menu", PANEL, "PIXEL.ScrollPanel")
---
+++ lua/pixelui/elements/cl_menu_option.lua
@@ -0,0 +1,132 @@
+
+local PANEL = {}
+
+AccessorFunc(PANEL, "m_pMenu", "Menu")
+AccessorFunc(PANEL, "m_bChecked", "Checked")
+AccessorFunc(PANEL, "m_bCheckable", "IsCheckable")
+
+AccessorFunc(PANEL, "Text", "Text", FORCE_STRING)
+AccessorFunc(PANEL, "TextAlign", "TextAlign", FORCE_NUMBER)
+AccessorFunc(PANEL, "Font", "Font", FORCE_STRING)
+
+PIXEL.RegisterFont("MenuOption", "Roboto Medium", 17)
+
+function PANEL:Init()
+    self:SetTextAlign(TEXT_ALIGN_LEFT)
+    self:SetFont("PIXEL.MenuOption")
+    self:SetChecked(false)
+
+
+    self.NormalCol = PIXEL.Colors.Scroller
+    self.HoverCol = PIXEL.OffsetColor(self.NormalCol, 15)
+
+    self.BackgroundCol = PIXEL.CopyColor(self.NormalCol)
+end
+
+function PANEL:SetIcon() end
+
+function PANEL:SetSubMenu(menu)
+    self.SubMenu = menu
+end
+
+function PANEL:AddSubMenu()
+    local SubMenu = vgui.Create("PIXEL.Menu", self)
+    SubMenu:SetVisible(false)
+    SubMenu:SetParent(self)
+
+    self:SetSubMenu(SubMenu)
+
+    return SubMenu
+end
+
+function PANEL:OnCursorEntered()
+    local parent = self.ParentMenu
+    if not IsValid(parent) then parent = self:GetParent() end
+    if not IsValid(parent) then return end
+
+    if not parent.OpenSubMenu then return end
+    parent:OpenSubMenu(self, self.SubMenu)
+end
+
+function PANEL:OnCursorExited() end
+
+function PANEL:Paint(w, h)
+    self.BackgroundCol = PIXEL.LerpColor(FrameTime() * 12, self.BackgroundCol, self:IsHovered() and self.HoverCol or self.NormalCol)
+
+    surface.SetDrawColor(self.BackgroundCol)
+    surface.DrawRect(0, 0, w, h)
+
+    PIXEL.DrawSimpleText(self:GetText(), self:GetFont(), PIXEL.Scale(14), h / 2, PIXEL.Colors.PrimaryText, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
+
+    if not self.SubMenu then return end
+    local dropBtnSize = PIXEL.Scale(8)
+    PIXEL.DrawImgur(w - dropBtnSize - PIXEL.Scale(6), h / 2 - dropBtnSize / 2, dropBtnSize, dropBtnSize, "gXg3U6X", PIXEL.Colors.PrimaryText)
+end
+
+function PANEL:OnPressed(mousecode)
+    self.m_MenuClicking = true
+end
+
+function PANEL:OnReleased(mousecode)
+    if not self.m_MenuClicking and mousecode == MOUSE_LEFT then return end
+    self.m_MenuClicking = false
+    CloseDermaMenus()
+end
+
+function PANEL:DoRightClick()
+    if self:GetIsCheckable() then
+        self:ToggleCheck()
+    end
+end
+
+function PANEL:DoClickInternal()
+    if self:GetIsCheckable() then
+        self:ToggleCheck()
+    end
+
+    if not self.m_pMenu then return end
+    self.m_pMenu:OptionSelectedInternal(self)
+end
+
+function PANEL:ToggleCheck()
+    self:SetChecked(not self:GetChecked())
+    self:OnChecked(self:GetChecked())
+end
+
+function PANEL:OnChecked(enabled) end
+
+function PANEL:CalculateWidth()
+    surface.SetFont(self:GetFont())
+    return surface.GetTextSize(self:GetText()) + PIXEL.Scale(34)
+end
+
+function PANEL:PerformLayout(w, h)
+    self:SetSize(math.max(self:CalculateWidth(), self:GetWide()), PIXEL.Scale(32))
+end
+
+vgui.Register("PIXEL.MenuOption", PANEL, "PIXEL.Button")
+
+PANEL = {}
+
+AccessorFunc(PANEL, "ConVar", "ConVar")
+AccessorFunc(PANEL, "ValueOn", "ValueOn")
+AccessorFunc(PANEL, "ValueOff", "ValueOff")
+
+function PANEL:Init()
+    self:SetChecked(false)
+    self:SetIsCheckable(true)
+    self:SetValueOn("1")
+    self:SetValueOff("0")
+end
+
+function PANEL:Think()
+    if not self.ConVar then return end
+    self:SetChecked(GetConVar(self.ConVar):GetString() == self.ValueOn)
+end
+
+function PANEL:OnChecked(checked)
+    if not self.ConVar then return end
+    RunConsoleCommand(self.ConVar, checked and self.ValueOn or self.ValueOff)
+end
+
+vgui.Register("PIXEL.MenuOptionCVar", PANEL, "PIXEL.MenuOption")
