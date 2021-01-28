636cd012505995aa52eb1ea492b8a8e0d410ee28
Tom O'Sullivan <tom@tomdotbat.dev>
2021-01-28 10:57:07 +0000

    Add a way to change the material settings of an imgur mat

--- lua/pixelui/common/cl_imgur.lua
+++ lua/pixelui/common/cl_imgur.lua
@@ -3,18 +3,18 @@ local materials = {}
 
 file.CreateDir("pixel")
 
-function PIXEL.GetImgur(id, callback, useproxy)
+function PIXEL.GetImgur(id, callback, useproxy, matSettings)
     if materials[id] then return callback(materials[id]) end
 
     if file.Exists("pixel/" .. id .. ".png", "DATA") then
-        materials[id] = Material("../data/pixel/" .. id .. ".png", "noclamp smooth")
+        materials[id] = Material("../data/pixel/" .. id .. ".png", matSettings or "noclamp smooth")
         return callback(materials[id])
     end
 
     http.Fetch(useproxy and "https://proxy.duckduckgo.com/iu/?u=https://i.imgur.com" or "https://i.imgur.com/" .. id .. ".png",
         function(body, len, headers, code)
             file.Write("pixel/" .. id .. ".png", body)
-            materials[id] = Material("../data/pixel/" .. id .. ".png", "noclamp smooth")
+            materials[id] = Material("../data/pixel/" .. id .. ".png", matSettings or "noclamp smooth")
             return callback(materials[id])
         end,
         function(error)
