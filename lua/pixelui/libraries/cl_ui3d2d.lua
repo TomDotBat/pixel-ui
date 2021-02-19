0e4a4ef889c412a4eea489038e0b89450483992d
Tom O'Sullivan <tom@tomdotbat.dev>
2021-02-19 11:28:59 +0000

    Remove UI3D2D cam.start prevention

--- lua/pixelui/libraries/cl_ui3d2d.lua
+++ lua/pixelui/libraries/cl_ui3d2d.lua
@@ -62,8 +62,6 @@ do --Rendering context creation and mouse position getters
         local isRendering
 
         function ui3d2d.startDraw(pos, angles, scale, ignoredEntity) --Starts a new 3d2d ui rendering context
-            if isRendering then print("[ui3d2d] Attempted to draw a new 3d2d ui without ending the previous one.") return end
-
             local eyePos = localPlayer:EyePos()
             if eyePos:DistToSqr(pos) > 400000 then return end
 
