88503d57657b7b612de2b22f7f4dcd7d4882cd72
Tom O'Sullivan <tom@tomdotbat.dev>
2021-02-26 13:43:41 +0000

    Share fonts internally to decrease wasted memory

--- lua/pixelui/common/cl_fonts.lua
+++ lua/pixelui/common/cl_fonts.lua
@@ -1,14 +1,30 @@
 
+PIXEL.UI.RegisteredFonts = PIXEL.UI.RegisteredFonts or {}
+local registeredFonts = PIXEL.UI.RegisteredFonts
+
+PIXEL.UI.SharedFonts = PIXEL.UI.SharedFonts or {}
+local sharedFonts = PIXEL.UI.SharedFonts
+
 function PIXEL.RegisterFontUnscaled(name, font, size, weight)
-    surface.CreateFont("PIXEL." .. name, {
+    weight = weight or 500
+
+    local identifier = font .. size .. weight
+    if sharedFonts[identifier] then return end
+
+    local fontName = "PIXEL:" .. identifier
+    registeredFonts[name] = fontName
+    sharedFonts[identifier] = true
+
+    surface.CreateFont(fontName, {
         font = font,
         size = size,
-        weight = weight or 500,
+        weight = weight,
         antialias = true
     })
 end
 
-local scaledFonts = {}
+PIXEL.UI.ScaledFonts = PIXEL.UI.ScaledFonts or {}
+local scaledFonts = PIXEL.UI.ScaledFonts
 
 function PIXEL.RegisterFont(name, font, size, weight)
     scaledFonts[name] = {
