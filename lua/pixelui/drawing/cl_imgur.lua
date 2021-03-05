8623656051f08d68218fc2e2c1ae44e9e982b1a6
Tom O'Sullivan <tom@tomdotbat.dev>
2021-03-05 11:22:21 +0000

    Heavily optimise imgur texture drawing

--- lua/pixelui/drawing/cl_imgur.lua
+++ lua/pixelui/drawing/cl_imgur.lua
@@ -1,53 +1,64 @@
 
-local progressMat = Material("error")
-PIXEL.GetImgur("635PPvg", function(mat) progressMat = mat end)
+local progressMat
+local getImgur = PIXEL.GetImgur
+getImgur("635PPvg", function(mat) progressMat = mat end)
 
-function PIXEL.DrawProgressWheel(x, y, w, h, col)
-    local progSize = math.min(w, h)
-    surface.SetMaterial(progressMat)
-    surface.SetDrawColor(col.r, col.g, col.b, col.a)
-    surface.DrawTexturedRectRotated(x + w / 2, y + h / 2, progSize, progSize, -CurTime() * 100)
+local curTime = CurTime
+local setMaterial = surface.SetMaterial
+local setDrawColor = surface.SetDrawColor
+local drawTexturedRectRotated = surface.DrawTexturedRectRotated
+local drawTexturedRect = surface.DrawTexturedRect
+
+local min = math.min
+local function drawProgressWheel(x, y, w, h, col)
+    local progSize = min(w, h)
+    setMaterial(progressMat)
+    setDrawColor(col.r, col.g, col.b, col.a)
+    drawTexturedRectRotated(x + w / 2, y + h / 2, progSize, progSize, -curTime() * 100)
 end
+PIXEL.DrawProgressWheel = drawProgressWheel
 
-local grabbingMaterials = {}
-local materials = {}
+do
+    local materials = {}
+    local grabbingMaterials = {}
 
-function PIXEL.DrawImgur(x, y, w, h, imgurId, col)
-    if not materials[imgurId] then
-        PIXEL.DrawProgressWheel(x, y, w, h, col)
+    function PIXEL.DrawImgur(x, y, w, h, imgurId, col)
+        if not materials[imgurId] then
+            drawProgressWheel(x, y, w, h, col)
 
-        if grabbingMaterials[imgurId] then return end
-        grabbingMaterials[imgurId] = true
+            if grabbingMaterials[imgurId] then return end
+            grabbingMaterials[imgurId] = true
 
-        PIXEL.GetImgur(imgurId, function(mat)
-            materials[imgurId] = mat
-            grabbingMaterials[imgurId] = nil
-        end)
+            getImgur(imgurId, function(mat)
+                materials[imgurId] = mat
+                grabbingMaterials[imgurId] = nil
+            end)
 
-        return
+            return
+        end
+
+        setMaterial(materials[imgurId])
+        setDrawColor(col.r, col.g, col.b, col.a)
+        drawTexturedRect(x, y, w, h)
     end
 
-    surface.SetMaterial(materials[imgurId])
-    surface.SetDrawColor(col.r, col.g, col.b, col.a)
-    surface.DrawTexturedRect(x, y, w, h)
-end
+    function PIXEL.DrawImgurRotated(x, y, w, h, rot, imgurId, col)
+        if not materials[imgurId] then
+            drawProgressWheel(x - w / 2, y - h / 2, w, h, col)
 
-function PIXEL.DrawImgurRotated(x, y, w, h, rot, imgurId, col)
-    if not materials[imgurId] then
-        PIXEL.DrawProgressWheel(x - w / 2, y - h / 2, w, h, col)
+            if grabbingMaterials[imgurId] then return end
+            grabbingMaterials[imgurId] = true
 
-        if grabbingMaterials[imgurId] then return end
-        grabbingMaterials[imgurId] = true
+            getImgur(imgurId, function(mat)
+                materials[imgurId] = mat
+                grabbingMaterials[imgurId] = nil
+            end)
 
-        PIXEL.GetImgur(imgurId, function(mat)
-            materials[imgurId] = mat
-            grabbingMaterials[imgurId] = nil
-        end)
+            return
+        end
 
-        return
+        setMaterial(materials[imgurId])
+        setDrawColor(col.r, col.g, col.b, col.a)
+        drawTexturedRectRotated(x, y, w, h, rot)
     end
-
-    surface.SetMaterial(materials[imgurId])
-    surface.SetDrawColor(col.r, col.g, col.b, col.a)
-    surface.DrawTexturedRectRotated(x, y, w, h, rot)
 end
