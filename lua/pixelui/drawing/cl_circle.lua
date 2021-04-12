fb05e22e0d8efb1676305abec8e09a64e4abadbd
blu <blu@bluiscool.dev>
2021-04-12 14:01:14 -0500

    refactor!: changed the circle draw functions

--- lua/pixelui/drawing/cl_circle.lua
+++ lua/pixelui/drawing/cl_circle.lua
@@ -1,57 +1,52 @@
 
-local materials = {
-    "4c5f5nk", --8
-    "mONPuyy", --16
-    "icx1Qbq", --32
-    "TpwrpKe", --64
-    "E8QbV5i", --128
-    "wAr5H1x", --256
-    "g52zxtK", --512
-    "9tHAUp6", --1024
-    "XAYX2uH" --2048
-}
+do
+    local materials = {
+        "4c5f5nk", --8
+        "mONPuyy", --16
+        "icx1Qbq", --32
+        "TpwrpKe", --64
+        "E8QbV5i", --128
+        "wAr5H1x", --256
+        "g52zxtK", --512
+        "9tHAUp6", --1024
+        "XAYX2uH" --2048
+    }
 
-local max = math.max
-function PIXEL.DrawCircle(x, y, w, h, col)
-    local size = max(w, h)
-    local id = materials[1]
+    local max = math.max
+    function PIXEL.DrawCircle(x, y, w, h, col)
+        local size = max(w, h)
+        local id = materials[1]
 
-    local curSize = 8
-    for i = 1, #materials do
-        if size <= curSize then break end
-        id = materials[i + 1] or id
-        curSize = curSize + curSize
+        local curSize = 8
+        for i = 1, #materials do
+            if size <= curSize then break end
+            id = materials[i + 1] or id
+            curSize = curSize + curSize
+        end
+
+        PIXEL.DrawImgur(x, y, w, h, id, col)
     end
-
-    PIXEL.DrawImgur(x, y, w, h, id, col)
 end
 
-local tableInsert = table.insert
-local mathRad, mathSin, mathCos = math.rad, math.sin, math.cos
-local surfaceSetDrawColor, surfaceDrawPoly = surface.SetDrawColor, surface.DrawPoly
-local drawNoTexture = draw.NoTexture
+do
+    local insert = table.insert
+    local rad, sin, cos = math.rad, math.sin, math.cos
 
-function PIXEL.CreateCircle(x, y, ang, seg, p, rad, color)
-    local circle = {}
-    circle.poly = {}
-    circle.color = color
+    function PIXEL.CreateCircle(x, y, ang, seg, pct, radius)
+        local circle = {}
 
-    tableInsert(circle.poly, {x = x, y = y})
+        insert(circle, {x = x, y = y})
 
-    for i = 0, seg do
-        local segdata = mathRad(((i / seg ) * -p + ang))
-        tableInsert(circle.poly, {x = x + mathSin(segdata) * rad, y = y + mathCos(segdata) * rad})
+        for i = 0, seg do
+            local segAngle = rad(((i / seg) * -pct + ang))
+            insert(circle, {x = x + sin(segAngle) * radius, y = y + cos(segAngle) * radius})
+        end
+
+        return circle
     end
-
-    return circle
 end
 
-function PIXEL.DrawCircleUncached(x, y, ang, seg, p, rad, color)
-    PIXEL.DrawCachedCircle(PIXEL.CreateCircle(x, y, ang, seg, p, rad, color))
-end
-
-function PIXEL.DrawCachedCircle(circle)
-    surfaceSetDrawColor(circle.color)
-    drawNoTexture()
-    surfaceDrawPoly(circle.poly)
+local drawPoly = surface.DrawPoly
+function PIXEL.DrawCircleUncached(x, y, ang, seg, pct, radius)
+    drawPoly(PIXEL.CreateCircle(x, y, ang, seg, pct, radius))
 end
