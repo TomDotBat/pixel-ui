1810fb9dfec19e61dc3e723a2a3aa48c0c2306c0
Tom O'Sullivan <tom@tomdotbat.dev>
2021-04-20 14:08:58 +0100

    feat: make PIXEL.FormatMoney DarkRP independant

--- lua/pixelui/common/sh_formatting.lua
+++ lua/pixelui/common/sh_formatting.lua
@@ -1,7 +1,50 @@
 
-local round = math.Round
-function PIXEL.FormatMoney(number)
-    return DarkRP.formatMoney(round(number))
+do
+    local currencySymbol = "$"
+    local currencyLeft = true
+
+    hook.Add("PostGamemodeLoaded", "PIXEL.UI.GetMoneyFormatSettings", function()
+        local config = (GM or GAMEMODE).Config
+        if not config then return end
+
+        if config.currency then currencySymbol = config.currency end
+        if config.currencyLeft then currencyLeft = config.currencyLeft end
+    end)
+
+    local function addCurrency(str)
+        return currencyLeft and (currencySymbol .. str) or (str .. currencySymbol)
+    end
+
+    do
+        local tostring = tostring
+        local find = string.find
+        local abs = math.abs
+        local round = math.Round
+
+        function PIXEL.FormatMoney(val)
+            if not val then return addCurrency("0") end
+
+            val = round(val)
+
+            if val >= 1e14 then return addCurrency(tostring(val)) end
+            if val <= -1e14 then return "-" .. addCurrency(tostring(abs(val))) end
+
+            local negative = val < 0
+
+            val = tostring(abs(val))
+            local dp = find(val, "%.") or #val + 1
+
+            for i = dp - 4, 1, -3 do
+                val = val:sub(1, i) .. "," .. val:sub(i + 1)
+            end
+
+            if val[#val - 1] == "." then
+                val = val .. "0"
+            end
+
+            return (negative and "-" or "") .. addCurrency(val)
+        end
+    end
 end
 
 local floor, format = math.floor, string.format
