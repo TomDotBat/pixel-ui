8e1aa302ff91ccdbb36bba09df437006ed3663af
Tom O'Sullivan <tom@tomdotbat.dev>
2021-06-25 09:45:56 +0100

    fix: missing PR commits (closes #18)

--- lua/autorun/sh_pixelui_loader.lua
+++ lua/autorun/sh_pixelui_loader.lua
@@ -20,29 +20,37 @@ PIXEL = PIXEL or {}
 PIXEL.UI = PIXEL.UI or {}
 PIXEL.UI.Version = "1.2.1"
 
-local function loadDirectory(dir)
-	local fil, fol = file.Find(dir .. "/*", "LUA")
+function PIXEL.LoadDirectory(path)
+	local files, folders = file.Find(path .. "/*", "LUA")
 
-	for k,v in ipairs(fil) do
-		local dirs = dir .. "/" .. v
+	for _, fileName in ipairs(files) do
+		path = path .. "/" .. fileName
 
-		if v:StartWith("cl_") then
-			if SERVER then AddCSLuaFile(dirs)
-			else include(dirs) end
-		elseif v:StartWith("sh_") then
-			AddCSLuaFile(dirs)
-			include(dirs)
+		if CLIENT then
+			include(path)
 		else
-			if SERVER then include(dirs) end
+			if fileName:StartWith("cl_") then
+				AddCSLuaFile(path)
+			elseif fileName:StartWith("sh_") then
+				AddCSLuaFile(path)
+				include(path)
+			else
+				include(path)
+			end
 		end
 	end
 
-	for k,v in pairs(fol) do
-		loadDirectory(dir .. "/" .. v)
+	return files, folders
+end
+
+function PIXEL.LoadDirectoryRecursive(basePath)
+	local _, folders = PIXEL.LoadDirectory(basePath)
+	for _, folderName in ipairs(folders) do
+		PIXEL.LoadDirectoryRecursive(basePath .. "/" .. folderName)
 	end
 end
 
-loadDirectory("pixelui")
+PIXEL.LoadDirectoryRecursive("pixelui")
 
 hook.Run("PIXEL.UI.FullyLoaded")
 
