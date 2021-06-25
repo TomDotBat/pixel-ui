97a59438fe3b1f6490f475c9f30fe5edfc60d868
Tom O'Sullivan <tom@tomdotbat.dev>
2021-06-25 09:43:35 +0100

    fix: missing PR commits from #11

--- lua/pixelui/core/cl_color.lua
+++ lua/pixelui/core/cl_color.lua
@@ -16,125 +16,88 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ]]
 
-local lerp = Lerp
-function PIXEL.LerpColor(t, from, to)
-    local newCol = Color(0, 0, 0)
+PIXEL.HSLToColor = HSLToColor
+PIXEL.ColorToHSL = ColorToHSL
 
-    newCol.r = lerp(t, from.r, to.r)
-    newCol.g = lerp(t, from.g, to.g)
-    newCol.b = lerp(t, from.b, to.b)
-    newCol.a = lerp(t, from.a, to.a)
+do
+    local format = string.format
+    function PIXEL.DecToHex(dec, zeros)
+        return format("%0" .. (zeros or 2) .. "x", dec)
+    end
 
-    return newCol
+    local max = math.max
+    local min = math.min
+    function PIXEL.ColorToHex(color)
+        return format("#%02X%02X%02X",
+            max(min(color.r, 255), 0),
+            max(min(color.g, 255), 0),
+            max(min(color.b, 255), 0)
+        )
+    end
 end
 
+local createColor = Color
 function PIXEL.CopyColor(col)
-    return Color(col.r, col.g, col.b, col.a)
+    return createColor(col.r, col.g, col.b, col.a)
 end
 
 function PIXEL.OffsetColor(col, offset)
-    return Color(col.r + offset, col.g + offset, col.b + offset)
+    return createColor(col.r + offset, col.g + offset, col.b + offset)
 end
 
-function PIXEL.Hue2RGB(p, q, t)
-    if t < 0 then t = t + 1 end
-    if t > 1 then t = t - 1 end
-    if t < 1 / 6 then return p + (q - p) * 6 * t end
-    if t < 1 / 2 then return q end
-    if t < 2 / 3 then return p + (q - p) * (2 / 3 - t) * 6 end
-    return p
-end
+do
+    local match = string.match
+    local tonumber = tonumber
 
-function PIXEL.HSLToColor(h, s, l, a)
-    local r, g, b
-    local t = h / (2 * math.pi)
-
-    if s == 0 then
-        r, g, b = l, l, l
-    else
-        local q
-        if l < 0.5 then
-        q = l * (1 + s)
-        else
-        q = l + s - l * s
-        end
-        local p = 2 * l - q
-
-        r = PIXEL.Hue2RGB(p, q, t + 1 / 3)
-        g = PIXEL.Hue2RGB(p, q, t)
-        b = PIXEL.Hue2RGB(p, q, t - 1 / 3)
-    end
-
-    return Color(r * 255, g * 255, b * 255, (a or 1) * 255)
-end
-
-function PIXEL.ColorToHSL(col)
-    local r = col.r / 255
-    local g = col.g / 255
-    local b = col.b / 255
-    local max, min = math.max(r, g, b), math.min(r, g, b)
-    b = max + min
-    local h = b / 2
-    if max == min then return 0, 0, h end
-    local s, l = h, h
-    local d = max - min
-    s = l > .5 and d / (2 - b) or d / b
-    if max == r then h = (g - b) / d + (g < b and 6 or 0)
-    elseif max == g then h = (b - r) / d + 2
-    elseif max == b then h = (r - g) / d + 4
-    end
-    return h * .16667, s, l
-end
-
-function PIXEL.DecToHex(d, zeros)
-    return string.format("%0" .. (zeros or 2) .. "x", d)
-end
-
-function PIXEL.RGBToHex(color)
-    return "#" ..
-        PIXEL.DecToHex(math.max(math.min(color.r, 255), 0)) ..
-        PIXEL.DecToHex(math.max(math.min(color.g, 255), 0)) ..
-        PIXEL.DecToHex(math.max(math.min(color.b, 255), 0))
-end
-
-function PIXEL.HexToRGB(hex)
-    hex = hex:gsub("#", "")
-
-    if (#hex == 3) then
-        local r = hex:sub(1, 1)
-        local g = hex:sub(2, 2)
-        local b = hex:sub(3, 3)
-
-        return Color(
-        tonumber("0x" .. r .. r),
-        tonumber("0x" .. g .. g),
-        tonumber("0x" .. b .. b)
+    function PIXEL.HexToColor(hex)
+        local r, g, b = match(hex, "#(..)(..)(..)")
+        return createColor(
+            tonumber(r, 16),
+            tonumber(g, 16),
+            tonumber(b, 16)
         )
     end
-
-    return Color(
-        tonumber("0x" .. hex:sub(1, 2)),
-        tonumber("0x" .. hex:sub(3, 4)),
-        tonumber("0x" .. hex:sub(5, 6))
-    )
 end
 
-local lastUpdate = 0
-local lastCol = Color(0, 0, 0)
-local HSVToColor = HSVToColor
-function PIXEL.GetRainbowColor()
-    local time = CurTime()
-    if lastUpdate == time then return lastCol end
+do
+    local curTime = CurTime
+    local hsvToColor = HSVToColor
 
-    lastUpdate = time
-    lastCol = HSVToColor((time * 50) % 360, 1, 1)
+    local lastUpdate = 0
+    local lastCol = createColor(0, 0, 0)
 
-    return lastCol
+    function PIXEL.GetRainbowColor()
+        local time = curTime()
+        if lastUpdate == time then return lastCol end
+
+        lastUpdate = time
+        lastCol = hsvToColor((time * 50) % 360, 1, 1)
+
+        return lastCol
+    end
+end
+
+do
+    local colorToHSL = ColorToHSL
+
+    function PIXEL.IsColorLight(col)
+        local _, _, lightness = colorToHSL(col)
+        return lightness >= .5
+    end
+end
+
+function PIXEL.LerpColor(t, from, to)
+    return createColor(from.r, from.g, from.b, from.a):Lerp(t, to)
+end
+
+function PIXEL.IsColorEqualTo(from, to)
+    return from.r == to.r and from.g == to.g and from.b == to.b and from.a == to.a
 end
 
 local colorMeta = FindMetaTable("Color")
-
 colorMeta.Copy = PIXEL.CopyColor
+colorMeta.IsLight = PIXEL.IsColorLight
+colorMeta.EqualTo = PIXEL.IsColorEqualTo
 
 function colorMeta:Offset(offset)
     self.r = self.r + offset
@@ -143,20 +106,11 @@ function colorMeta:Offset(offset)
     return self
 end
 
+local lerp = Lerp
 function colorMeta:Lerp(t, to)
     self.r = lerp(t, self.r, to.r)
     self.g = lerp(t, self.g, to.g)
     self.b = lerp(t, self.b, to.b)
     self.a = lerp(t, self.a, to.a)
     return self
-end
-
-local colorToHSL = PIXEL.ColorToHSL
-function colorMeta:IsLight()
-    local _, _, lightness = colorToHSL(self)
-    return lightness >= .5
-end
-
-function colorMeta:EqualTo(to)
-    return self.r == to.r and self.g == to.g and self.b == to.b and self.a == to.a
 end
