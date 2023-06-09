6b9534e2c0a694d056f5826e413ad8e5bb68fff5
Lythium <62567544+Lythium4848@users.noreply.github.com>
2023-06-09 23:36:13 +0100

    feat: Imgur queue system

--- lua/pixelui/core/cl_imgur.lua
+++ lua/pixelui/core/cl_imgur.lua
@@ -18,17 +18,54 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 local materials = {}
 
+local imgurQueue = {}
+
+function imgurQueue.new()
+    return { coroutines = {} }
+end
+
+function imgurQueue.enqueue(queue, func, ...)
+    local co = coroutine.create(func)
+    table.insert(queue.coroutines, { co = co, args = {...} })
+end
+
+function imgurQueue.process(queue)
+    if queue.processing then return end
+    queue.processing = true
+
+    while table.Count(queue.coroutines) >= 0 do
+        if table.Count(queue.coroutines) == 0 then
+            queue.processing = false
+            return
+        end
+
+        local coroutineData = table.remove(queue.coroutines, 1)
+        local co, args = coroutineData.co, coroutineData.args
+        local success, _ = coroutine.resume(co, unpack(args))
+
+        if success and coroutine.status(co) == "suspended" then
+            table.insert(queue.coroutines, coroutineData)
+        end
+    end
+end
+
 file.CreateDir("pixel")
 
-function PIXEL.GetImgur(id, callback, useproxy, matSettings)
+local function downloadIcon(data)
+    local id = data[1]
+    local callback = data[2]
+
     if materials[id] then return callback(materials[id]) end
+
+    local useproxy = data[3]
+    local matSettings = data[4]
 
     if file.Exists("pixel/" .. id .. ".png", "DATA") then
         materials[id] = Material("../data/pixel/" .. id .. ".png", matSettings or "noclamp smooth mips")
         return callback(materials[id])
     end
 
-    http.Fetch(useproxy and "https://proxy.duckduckgo.com/iu/?u=https://i.imgur.com" or "https://i.imgur.com/" .. id .. ".png",
+    http.Fetch((useproxy and "https://proxy.duckduckgo.com/iu/?u=https://i.imgur.com/" or "https://i.imgur.com/") .. id .. ".png",
         function(body, len, headers, code)
             if len > 2097152 then
                 materials[id] = Material("nil")
@@ -43,9 +80,18 @@ function PIXEL.GetImgur(id, callback, useproxy, matSettings)
         function(error)
             if useproxy then
                 materials[id] = Material("nil")
-                return callback(materials[id])
+                return callback(materials[id], error)
             end
             return PIXEL.GetImgur(id, callback, true)
         end
     )
+end
+
+local downloadQueue = imgurQueue.new()
+
+function PIXEL.GetImgur(id, callback, useproxy, matSettings)
+    if materials[id] then return callback(materials[id]) end
+    imgurQueue.enqueue(downloadQueue, downloadIcon, { id, callback, useproxy, matSettings })
+
+    imgurQueue.process(downloadQueue)
 end
