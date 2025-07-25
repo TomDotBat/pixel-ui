62f7e0bc1801021764de0e3e42341f3c1aee425c
Tom O'Sullivan <tom@tomdotbat.dev>
2025-07-25 14:04:01 +0100

    chore: bump version

--- lua/autorun/sh_pixelui_loader.lua
+++ lua/autorun/sh_pixelui_loader.lua
@@ -17,7 +17,7 @@
 
 PIXEL = PIXEL or {}
 PIXEL.UI = PIXEL.UI or {}
-PIXEL.UI.Version = "1.3.2"
+PIXEL.UI.Version = "1.4.1"
 
 function PIXEL.LoadDirectory(path)
 	local files, folders = file.Find(path .. "/*", "LUA")
